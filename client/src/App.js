8c21a90e1b747aac5704402fc3a9bda307470a55
xxun_jh <ths3630802@naver.com>
2021-07-27 15:20:47 +0900

    카카오맵 api 연동

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+/*global kakao*/
 import './App.css';
+import React, { useEffect } from 'react'
+const { MapApiKey } = require('./config/key');
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    debugger
+    const script = document.createElement("script");
+    script.async = true;
+    script.src =
+      `https://dapi.kakao.com/v2/maps/sdk.js?appkey=${MapApiKey.key}&autoload=false`;
+    document.head.appendChild(script);
+
+    const onLoadKakaoMap = () => {
+      window.kakao.maps.load(() => {
+        const mapContainer = document.getElementById("map");
+        const mapOption = {
+          center: new window.kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+          level: 3, // 지도의 확대 레벨
+        };
+        new window.kakao.maps.Map(mapContainer, mapOption);
+      });
+    };
+    script.addEventListener("load", onLoadKakaoMap);
+
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="map" style={{ width: "500px", height: "400px" }}></div>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ client/src/config/prod.js
@@ -0,0 +1,3 @@
+const MapApiKey = { key: process.env.REACT_APP_KAKAO_MAP_APP_KEY }
+
+module.exports = { MapApiKey }
